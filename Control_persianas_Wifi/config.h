aa19e31287922d763878c68eceecff19ed70e059
Cristian <51058790+cgasper79@users.noreply.github.com>
2021-11-04 17:53:42 +0100

    Update config.h

--- Control_persianas_Wifi/config.h
+++ Control_persianas_Wifi/config.h
@@ -1,6 +1,6 @@
-const char* ssid     = "Cris_Monike";
-const char* password = "gascoing1979";
-const char* hostname = "ESP32_Persianas";
+const char* ssid     = "TU_SSID";
+const char* password = "TU_PASSWORD";
+const char* hostname = "TU_NOMBRE";
 
 IPAddress ip(192, 168, 1, 200);
 IPAddress gateway(192, 168, 1, 200);
